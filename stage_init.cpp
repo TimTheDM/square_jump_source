ed20d1c04f5b35516279372fe2549fc41f7d37cb
Zerminar12 <zerminar12@gmail.com>
2021-03-20 21:09:06 -0400

    Completed primary route of the pitfall level.

--- stage_init.cpp
+++ stage_init.cpp
@@ -8,7 +8,7 @@ std::vector<fixture>* stageInit() {
   std::vector<fixture>* stages = new std::vector<fixture>[STAGE_TOTAL];
   static sf::Texture spikes;
   spikes.loadFromFile("spikes.png");
-  stages[0] = collisionPlayground(spikes);
+  stages[0] = easyCarson(spikes);
   return stages;
 }
 
@@ -81,4 +81,97 @@ std::vector<fixture> collisionPlayground(sf::Texture& spike) {
   cPlayground.push_back(fix17);
   cPlayground.push_back(fix18);
   return cPlayground;
+}
+
+std::vector<fixture> easyCarson(sf::Texture& spike) {
+  std::vector<fixture> pitfall;
+  fixture victory(false, true, spike, 50, 50, 675, 675);
+  victory.platform.setFillColor(sf::Color(255,255,255));
+  fixture victoryFlag(false, false, spike, 25, 100, 650, 675);
+  fixture optionalVictory(false, true, spike, 25, 25, 450, 750);
+  optionalVictory.platform.setFillColor(sf::Color(255,0,0));
+
+  fixture floor(false, false, spike, 1200, 25, 0, 775);
+  fixture leftWall(false, false, spike, 25, 1200, 0, 0);
+  fixture rightWall(false, false, spike, 25, 1200, 1175, 0);
+  fixture towerBase(false, false, spike, 25, 1200, 600, 300);
+
+  fixture rightPlat1(false, false, spike, 275, 25, 350, 500);
+  fixture rightPlat2(false, false, spike, 50, 25, 550, 400);
+
+  fixture leftPlat1(false, false, spike, 100, 25, 0, 600);
+  fixture leftPlat2(false, false, spike, 200, 25, 0, 350);
+  fixture leftPlat3(false, false, spike, 200, 25, 0, 295);
+  fixture leftPlat4(false, false, spike, 100, 25, 0, 150);
+
+  fixture floatPlat(false, false, spike, 60, 25, 545, 150);
+  fixture floatSpike1(true, false, spike, 50, 50, 600, 225);
+  floatSpike1.hazard.rotate(180);
+  fixture floatSpike2(true, false, spike, 50, 50, 550, 100);
+
+  fixture maze1(false, false, spike, 400, 25, 600, 275);
+  fixture maze2(false, false, spike, 400, 25, 775, 425);
+  fixture maze3(false, false, spike, 500, 25, 600, 550);
+  fixture maze4(false, false, spike, 25, 300, 775, 600);
+  fixture maze5 (false, false, spike, 50, 25, 830, 670);
+
+  fixture mazeSpike1(true, false, spike, 50, 50, 950, 375);
+  fixture mazeSpike2(true, false, spike, 50, 50, 900, 350);
+  mazeSpike2.hazard.rotate(180);
+  fixture mazeSpike3(true, false, spike, 50, 50, 1100, 500);
+  mazeSpike3.hazard.rotate(180);
+  fixture mazeSpike4(true, false, spike, 50, 50, 1125, 725);
+  fixture mazeSpike5(true, false, spike, 50, 50, 1100, 725);
+  fixture mazeSpike6(true, false, spike, 50, 50, 900, 625);
+  mazeSpike6.hazard.rotate(180);
+
+  fixture leftSidePit1(true, false, spike, 50, 50, 150, 725);
+  fixture leftSidePit2(true, false, spike, 50, 50, 200, 725);
+  fixture leftSidePit3(true, false, spike, 50, 50, 250, 725);
+  fixture leftSidePit4(true, false, spike, 50, 50, 325, 725);
+  fixture leftSidePit5(true, false, spike, 50, 50, 375, 725);
+  fixture leftSidePit6(true, false, spike, 50, 50, 500, 725);
+  fixture leftSidePit7(true, false, spike, 50, 50, 550, 725);
+
+  pitfall.push_back(victory);
+  pitfall.push_back(victoryFlag);
+  pitfall.push_back(optionalVictory);
+  pitfall.push_back(floor);
+  pitfall.push_back(leftWall);
+  pitfall.push_back(rightWall);
+  pitfall.push_back(towerBase);
+
+  pitfall.push_back(leftPlat1);
+  pitfall.push_back(leftPlat2);
+  pitfall.push_back(leftPlat3);
+  pitfall.push_back(leftPlat4);
+
+  pitfall.push_back(rightPlat1);
+  pitfall.push_back(rightPlat2);
+
+  pitfall.push_back(floatPlat);
+  pitfall.push_back(floatSpike1);
+  pitfall.push_back(floatSpike2);
+
+  pitfall.push_back(maze1);
+  pitfall.push_back(maze2);
+  pitfall.push_back(maze3);
+  pitfall.push_back(maze4);
+  pitfall.push_back(maze5);
+
+  pitfall.push_back(mazeSpike1);
+  pitfall.push_back(mazeSpike2);
+  pitfall.push_back(mazeSpike3);
+  pitfall.push_back(mazeSpike4);
+  pitfall.push_back(mazeSpike5);
+  pitfall.push_back(mazeSpike6);
+
+  pitfall.push_back(leftSidePit1);
+  pitfall.push_back(leftSidePit2);
+  pitfall.push_back(leftSidePit3);
+  pitfall.push_back(leftSidePit4);
+  pitfall.push_back(leftSidePit5);
+  pitfall.push_back(leftSidePit6);
+  pitfall.push_back(leftSidePit7);
+  return pitfall;
 }
